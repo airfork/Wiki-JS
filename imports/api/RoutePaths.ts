788e5c91b4e85c82d27f76afe637ecdd4202f635
Tunji Afolabi-Brown <airfork@gmail.com>
2020-08-03 22:17:48 -0400

    Made the route objects their own class

--- imports/api/RoutePaths.ts
+++ imports/api/RoutePaths.ts
@@ -2,17 +2,21 @@
 import { Wiki } from '/imports/ui/Wiki';
 import { Index } from '/imports/ui/Index'
 
+class Route {
+  readonly routeName: String;
+  readonly path: String;
+  readonly component: () => JSX.Element;
+
+  constructor(routeName: String, path: String, component: () => JSX.Element) {
+    this.routeName = routeName;
+    this.path = path;
+    this.component = component;
+  }
+}
+
 const Routes = {
-  wikiMain: {
-    routeName: 'wiki',
-    path: '/wiki',
-    component: Wiki
-  },
-  index: {
-    routeName: 'index',
-    path: '/',
-    component: Index
-  },
+  wikiMain: new Route('wiki', '/wiki', Wiki),
+  index: new Route('index', '/', Index),
 }
 
 export default Routes;
