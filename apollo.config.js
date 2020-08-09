18c39f47947cc766ad45374197c03d4e3b5f8e08
Zedjones <dojoman19@gmail.com>
2020-08-08 22:20:28 -0400

    Added initial Apollo config

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+exports.client = {
+  includes: ['./src/**/*.tsx', './src/**/*.ts'],
+  excludes: [
+    '**/node_modules'
+  ],
+  target: 'typescript',
+  tagName: 'gql',
+  service: {
+    name: 'wiki-js',
+    localSchemaFile: 'src/graphql/schema.graphql'
+  }
+}
