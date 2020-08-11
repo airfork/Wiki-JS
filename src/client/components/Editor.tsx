916b7da9e3368bdbb8e44627ea5c848ace43aefe
Tunji Afolabi-Brown <airfork@gmail.com>
2020-08-11 11:24:00 -0400

    changed editor config

--- src/client/components/Editor.tsx
+++ src/client/components/Editor.tsx
@@ -6,7 +6,24 @@ const ReactQuill = typeof window === 'object' ? require('react-quill') : () => f
 export default function Editor() {
   const [value, setValue] = useState('');
 
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+
   return (
-    <ReactQuill theme="snow" value={value} onChange={setValue} />
+    <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}
+                formats={formats} />
   );
 }
