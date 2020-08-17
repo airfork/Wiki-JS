d641d50f41779793ee29f08ff2622782995f3dfd
Zedjones <dojoman19@gmail.com>
2020-08-16 21:09:58 -0400

    Changed link to /signup

--- src/client/components/LoginForm.tsx
+++ src/client/components/LoginForm.tsx
@@ -153,7 +153,7 @@ export default function LoginForm(props: LoginFormProps) {
                   </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
