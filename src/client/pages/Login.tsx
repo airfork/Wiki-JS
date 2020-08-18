1afae1a61eec06f2497641f9e930231d481e3912
Tunji Afolabi-Brown <airfork@gmail.com>
2020-08-18 14:43:23 -0400

    search bar

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -3,7 +3,5 @@ import { Container } from '@material-ui/core';
 import LoginForm from "../components/LoginForm";
 
 export const Login = () => (
-  <Container maxWidth={"sm"}>
     <LoginForm />
-  </Container>
 );
